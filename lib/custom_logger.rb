d467ff9a1417e36ef1d93f7599eedce97b45c489
Ilya <simonewarlet@gmail.com>
2025-01-27 12:01:50 +0200

    Fix log message not working

--- lib/custom_logger.rb
+++ lib/custom_logger.rb
@@ -1,33 +1,14 @@
 # frozen_string_literal: true
 
-require_relative "custom_logger/version"
+require_relative "custom_logger/railtie"
 
 module CustomLogger
   class Error < StandardError; end
 
   class Logger
-    def self.log_message(message = '
-                                      ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-                                      ░░░░░░░░░░░░░▄▄▄▄▄▄▄░░░░░░░░░
-                                      ░░░░░░░░░▄▀▀▀░░░░░░░▀▄░░░░░░░
-                                      ░░░░░░░▄▀░░░░░░░░░░░░▀▄░░░░░░
-                                      ░░░░░░▄▀░░░░░░░░░░▄▀▀▄▀▄░░░░░
-                                      ░░░░▄▀░░░░░░░░░░▄▀░░██▄▀▄░░░░
-                                      ░░░▄▀░░▄▀▀▀▄░░░░█░░░▀▀░█▀▄░░░
-                                      ░░░█░░█▄▄░░░█░░░▀▄░░░░░▐░█░░░
-                                      ░░▐▌░░█▀▀░░▄▀░░░░░▀▄▄▄▄▀░░█░░
-                                      ░░▐▌░░█░░░▄▀░░░░░░░░░░░░░░█░░
-                                      ░░▐▌░░░▀▀▀░░░░░░░░░░░░░░░░▐▌░
-                                      ░░▐▌░░░░░░░░░░░░░░░▄░░░░░░▐▌░
-                                      ░░▐▌░░░░░░░░░▄░░░░░█░░░░░░▐▌░
-                                      ░░░█░░░░░░░░░▀█▄░░▄█░░░░░░▐▌░
-                                      ░░░▐▌░░░░░░░░░░▀▀▀▀░░░░░░░▐▌░
-                                      ░░░░█░░░░░░░░░░░░░░░░░░░░░█░░
-                                      ░░░░▐▌▀▄░░░░░░░░░░░░░░░░░▐▌░░
-                                      ░░░░░█░░▀░░░░░░░░░░░░░░░░▀░░░
-                                      ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-    ')
-      Rails.logger.info message
+    def self.log_message(message = "eaweawewaeaw")
+      logger = ActiveSupport::Logger.new($stdout)
+      logger.info message
     end
   end
 end
