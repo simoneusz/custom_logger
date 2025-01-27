7b685a26325c2c60081cb4632275ee7e22b401df
Ilya <simonewarlet@gmail.com>
2025-01-27 13:10:29 +0200

    Remove comments

---
+++ spec/rails/commands/log_message_command_spec.rb
@@ -0,0 +1,18 @@
+require "rails/command"
+require "rails/commands/log_message/log_message_command"
+require "spec_helper"
+
+RSpec.describe Rails::Command::LogMessageCommand do
+  subject(:logger) { CustomLogger::Logger }
+  describe "#perform" do
+    it "logs the provided message" do
+      expect(logger).to receive(:log_message).with("Hello, World!")
+      Rails::Command::LogMessageCommand.new.perform("Hello, World!")
+    end
+
+    it "logs the default message with no arguments" do
+      expect(logger).to receive(:log_message).with("Default log message")
+      Rails::Command::LogMessageCommand.new.perform
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,7 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
