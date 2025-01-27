44b12a279f7e0673a7c42b4f563fa9429dd843cf
Ilya <simonewarlet@gmail.com>
2025-01-27 10:49:04 +0200

    Initial commit

---
+++ lib/custom_logger/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CustomLogger
+  VERSION = "0.1.0"
+end
---
+++ spec/custom_logger_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CustomLogger do
+  it "has a version number" do
+    expect(CustomLogger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
