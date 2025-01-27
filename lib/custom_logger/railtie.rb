fb2500cb20ad5113ddc1e2a3e87a5fddfc02f57c
Ilya <simonewarlet@gmail.com>
2025-01-27 15:00:39 +0200

    Add logginf messager from config in rails initializers

--- lib/custom_logger/railtie.rb
+++ lib/custom_logger/railtie.rb
@@ -3,10 +3,20 @@
 require "rails/railtie"
 
 module CustomLogger
-  # Class for integration with rails
+  # Class for custom logging in rails commands
   class Railtie < Rails::Railtie
-    rake_tasks do
-      load "task/custom_logger.rake"
+    initializer "custom_logger.configure_rails_initialization" do
+      messages = Rails.configuration.custom_logger
+
+      CustomLogger::Logger.log_message messages['startup_message']
+
+      if defined?(Rails::Console)
+        CustomLogger::Logger.log_message messages['console_message']
+      end
+
+      if defined?(Rails::Server)
+        CustomLogger::Logger.log_message "Starting Rails server..."
+      end
     end
   end
 end
